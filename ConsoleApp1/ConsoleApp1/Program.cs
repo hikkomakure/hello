ae4ad84bbc91c901b33dd2163948e919770d2060
dispensary <hikkomakure@yandex.ru>
2025-02-27 22:37:43 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите свое имя");
+        int name = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите свой возраст");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите свой город");
+        int gorod = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"привет {name}, тебе {age} и ты проживаешь в этом {gorod}");
+    }
+
+}
+
+
