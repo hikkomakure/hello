415e5d9dfdbdf9c83f84c0c19788c720a70dc503
dispensary <hikkomakure@yandex.ru>
2025-02-28 22:34:19 +0300

    Add files via upload

---
+++ hello my friends/hi/hi/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите свое имя");
+        string name = Console.ReadLine();
+        Console.WriteLine("Введите свой возраст"); 
+        string age = Console.ReadLine(); 
+        Console.WriteLine("Введите свой город"); 
+        string gorod = Console.ReadLine(); 
+        Console.WriteLine($"привет {name}, тебе {age} и ты проживаешь в этом {gorod}");
+
+    }
+
+}
